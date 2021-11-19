a264a2fa5c15d0f3f08729c6a339ce3a9b50ca30
Thibaut <tdumalin@azuracom.com>
2021-11-19 16:46:58 +0100

    multiple values for booleanType

--- DataTransformer/BooleanTransformer.php
+++ DataTransformer/BooleanTransformer.php
@@ -7,20 +7,43 @@ use Azuracom\SpreadsheetToObject\Exception\TransformationFailedException;
 
 class BooleanTransformer implements DataTransformerInterface
 {
+    /** @var array */
+    private $trueValues;
+
+    /** @var array */
+    private $falseValues;
+
+    public function __construct(array $trueValues = [1, '1', 'yes', 'y'], array $falseValues = [0, '0', 'no', 'n'])
+    {
+        $this->trueValues = $trueValues;
+        $this->falseValues = $falseValues;
+    }
     public function transform($boolvalue)
     {
         return $boolvalue ? 1 : 0;
     }
 
     public function reverseTransform($stringValue)
-     {
+    {
         if ($stringValue === null) {
             return null;
         }
-        if ($stringValue != 1 && $stringValue != 0) {
-            throw new TransformationFailedException("azuracom_spreadsheet_to_object.data_transformer_exception.boolean");
+
+        foreach ($this->trueValues as $value) {
+            if ($stringValue === $value) {
+                return true;
+            }
         }
 
-        return $stringValue == 1 ? true : false;
+        foreach ($this->falseValues as $value) {
+            if ($stringValue === $value) {
+                return false;
+            }
+        }
+
+        throw new TransformationFailedException("azuracom_spreadsheet_to_object.data_transformer_exception.boolean", 0, null, [
+            '%true_values%' => '"'.implode('", "', $this->trueValues).'"',
+            '%false_values%' => '"'.implode('", "', $this->falseValues).'"',
+        ]);
     }
 }
