05b91a0cfb4cf9ab2aa806e812738ce945458c95
Thibaut <tdumalin@azuracom.com>
2022-07-19 11:36:37 +0200

    EnumType + exceldate format

---
+++ ColumnType/EnumType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Azuracom\SpreadsheetToObject\ColumnType;
+
+use Azuracom\SpreadsheetToObject\DataTransformer\EnumTransformer;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EnumType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+        $resolver
+            ->setDefaults([
+                'not_found_message' => "azuracom_spreadsheet_to_object.data_transformer_exception.choice_value_not_found",
+                'enum_label' => function ($enum) {
+                    return  method_exists($enum, 'label') ? $enum->label() : $enum->name;
+                },
+            ])
+            ->setRequired(['class'])
+            ->setAllowedTypes('class', 'string')
+            ->setAllowedValues('class', \Closure::fromCallable('enum_exists'));
+    }
+
+    public function getDefaultTransformer($options): ?DataTransformerInterface
+    {
+        return new EnumTransformer($options['class'], $options['enum_label'], $options['not_found_message']);
+    }
+}
