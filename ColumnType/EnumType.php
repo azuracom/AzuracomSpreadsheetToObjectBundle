cde28d8c27a94129a5873726814c85c5c6ea9405
Thibaut <tdumalin@azuracom.com>
2023-04-24 17:15:27 +0200

    Add enum translation support

--- ColumnType/EnumType.php
+++ ColumnType/EnumType.php
@@ -5,9 +5,17 @@ namespace Azuracom\SpreadsheetToObject\ColumnType;
 use Azuracom\SpreadsheetToObject\DataTransformer\EnumTransformer;
 use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class EnumType extends AbstractType
 {
+    private $translator;
+
+    public function __construct(?TranslatorInterface $translator = null)
+    {
+        $this->translator = $translator;
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         parent::configureOptions($resolver);
@@ -15,7 +23,12 @@ class EnumType extends AbstractType
             ->setDefaults([
                 'not_found_message' => "azuracom_spreadsheet_to_object.data_transformer_exception.choice_value_not_found",
                 'enum_label' => function ($enum) {
-                    return  method_exists($enum, 'label') ? $enum->label() : $enum->name;
+                    $label =  method_exists($enum, 'label') ? $enum->label() : $enum->name;
+                    if ($this->translator) {
+                        $label = $this->translator->trans($label);
+                    }
+
+                    return $label;
                 },
             ])
             ->setRequired(['class'])
