a3c916c746e1ae90c089b4e4304818746717aceb
Thibaut <tdumalin@azuracom.com>
2023-05-16 09:16:49 +0200

    ExportColumnGroupType: options entry_options

--- Form/Type/ExportColumnGroupType.php
+++ Form/Type/ExportColumnGroupType.php
@@ -5,31 +5,42 @@ namespace Azuracom\SpreadsheetToObject\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ExportColumnGroupType extends AbstractType
 {
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('check', CheckboxType::class,[
+            ->add('check', CheckboxType::class, [
                 'label' => $options['label'],
                 'required' => false,
-            ]);
-        $subBuilder = $builder->getFormFactory()->createNamedBuilder('children', $options['entry_type'], null, [
-            'column_key' => $options['column_key'],
-            'label'=> false,
-        ]);
-        $builder->add($subBuilder);
+            ])
+            ->add('children', $options['entry_type'], $options['entry_options']);
     }
 
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefault('label','Check all');
-        $resolver->setRequired(['column_key', 'entry_type']);
-        $resolver->setAllowedTypes('entry_type', 'string');
-        $resolver->setAllowedTypes('column_key', ['string', 'null']);
+        $resolver
+            ->setDefined('entry_type')
+            ->setRequired('entry_type')
+            ->setAllowedTypes('entry_type', 'string');
+
+        $resolver
+            ->setDefault('column_key', null)
+            ->setAllowedTypes('column_key', ['string', 'null']);
+
+        $resolver
+            ->setDefault('entry_options', [
+                'label' => false,
+            ])
+            ->setAllowedTypes('entry_options', 'array')
+            ->setNormalizer('entry_options',  function (Options $options, $value) {
+                return array_merge([
+                    'column_key' => $options['column_key'],
+                ], $value);
+            });
     }
 }
